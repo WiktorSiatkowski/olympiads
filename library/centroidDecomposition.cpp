da9cbc121259f6ba5e06d84513b0a02d4c8a11be
WiktorSiatkowski <wik.siatprywatnie@gmail.com>
2020-08-13 21:52:27 +0200

    centroiddecomposition code framework refactor

--- library/centroidDecomposition.cpp
+++ library/centroidDecomposition.cpp
@@ -1,55 +1,57 @@
 #include<bits/stdc++.h>
+#define ST first
+#define ND second
 using namespace std;
 const int SIZE = 1e5 + 20;
 
-int n,k,centroid,res;
-vector<pair<int,int> > adj[SIZE];
+int n,k,centroid,res,treesize;
+vector<pair<int,int> > adj[SIZE], toAdd;
 bool dead[SIZE];
-int subtree[SIZE];
-vector<pair<int,int> > toAdd;
-int maxOdl[SIZE];
+int subtree[SIZE],maxOdl[SIZE];
 
-void subtreeSize(int v, int prev){
+void dfs1(int v, int prev){
+	treesize++;
 	subtree[v] = 1;
-	for(auto it : adj[v]) 
-		if(!dead[it.first] && it.first != prev){
-			subtreeSize(it.first,v);
-			subtree[v] += subtree[it.first];
-		} 
-}
-
-void findCentroid(int v, int prev, int treeSize){
-	int which = -1;
-	for(auto it : adj[v])
-		if(!dead[it.first] && it.first != prev && subtree[it.first] * 2 > treeSize) 
-			which = it.first;
-	if(which == -1)
-		centroid = v;
-	else
-		findCentroid(which,v,treeSize);
-}
-
-void kPaths(int v, int prev, int level, int sum){
-	if(level <= k) res = max(res,sum + maxOdl[k - level]);
 	for(auto it : adj[v]){
-		if(!dead[it.first] && it.first != prev) 
-			kPaths(it.first,v,level+1,sum + it.second);
+		if(dead[it.ST] || it.ST == prev) continue;
+		dfs1(it.ST,v);
+		subtree[v] += subtree[it.ST];
+	}
+}
+
+void dfs2(int v, int prev, int x){
+	int maks = x - subtree[v];
+	for(auto it : adj[v]){
+		if(it.ST == prev || dead[it.ST]) continue;
+		dfs2(it.ST,v,x);
+		maks = max(maks,subtree[it.ST]);	
+	}
+	if(maks <= x / 2) centroid = v;
+}
+
+void dfs3(int v, int prev, int dep, int sum){
+	if(dep <= k){
+		res = max(res,sum + maxOdl[k - dep]);
+		toAdd.push_back({dep,sum});
+	}
+	for(auto it : adj[v]){
+		if(dead[it.ST] || it.ST == prev) continue;
+		dfs3(it.ST,v,dep+1,sum + it.ND);
 		if(v == centroid){
-			for(auto it2 : toAdd) maxOdl[it2.first] = max(maxOdl[it2.first],it2.second);
+			for(auto it2 : toAdd) maxOdl[it2.ST] = max(maxOdl[it2.ST],it2.ND);
 			toAdd.clear();
 		}	
 	}
-	if(level <= k) toAdd.push_back({level,sum});
 }
 
-void findFor(int v){
-	for(int i = 0; i <= k; i++) maxOdl[i] = 0;
-	centroid = -1;
-	subtreeSize(v,-1);
-	findCentroid(v,-1,subtree[v]);
-	kPaths(centroid,-1,0,0);
+void decompose(int v){
+	treesize = 0;
+	dfs1(v,-1);
+	for(int i = 0; i <= treesize; i++) maxOdl[i] = 0;
+	dfs2(v,-1,subtree[v]);
+	dfs3(centroid,-1,0,0);
 	dead[centroid] = 1;
-	for(auto it : adj[centroid]) if(!dead[it.first]) findFor(it.first);
+	for(auto it : adj[centroid]) if(!dead[it.first]) decompose(it.first);
 }
 
 int main(){
@@ -62,8 +64,7 @@ int main(){
 		adj[y].push_back({x,z});
 	}
 
-	findFor(1);
+	decompose(1);
 
 	cout << res;
-
 }
